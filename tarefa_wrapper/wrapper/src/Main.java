3e61be1fbcef03ff3a39f0beec278d0a0cb6e38e
Consoli310 <matheusconsoli310@gmail.com>
2024-08-01 14:41:53 -0300

    fisrt commit

---
+++ tarefa_wrapper/wrapper/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int primitiva = sc.nextInt();
+        System.out.println(primitiva);
+
+        Integer wrapper = primitiva;
+        System.out.println(wrapper);
+        }
+    }

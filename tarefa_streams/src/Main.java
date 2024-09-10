7f66ffea89ab0e0632877946bc7acfd7e180f0d2
Consoli310 <matheusconsoli310@gmail.com>
2024-09-10 14:15:32 -0300

    first commit

---
+++ tarefa_streams/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        List<String> lista = new ArrayList<>();
+
+        System.out.println("Digite o nome e sexo das pessoas, separadas por vírgula");
+        System.out.println("Exemplo: Marcio-m, Mariana-f, Eduardo-m, Luana-f");
+
+        String nomesJuntos = sc.nextLine();
+
+        String[] nomeSeparados = nomesJuntos.split(",");
+
+        for(String nomes : nomeSeparados){
+            lista.add(nomes);
+        }
+
+        List<String> listaMulheres = lista.stream()
+                .filter(p -> p.split("-")[1].equals("f"))
+                .toList();
+
+        System.out.println("Lista das mulheres: ");
+        listaMulheres.forEach(System.out::println);
+        sc.close();
+        
+    }
+}

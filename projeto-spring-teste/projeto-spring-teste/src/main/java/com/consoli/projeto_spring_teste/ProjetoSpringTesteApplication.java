44be19f293cc9f3d3d5ebe192eaca328d5ca6847
Consoli310 <matheusconsoli310@gmail.com>
2024-10-21 14:05:22 -0300

    first commit

---
+++ projeto-spring-teste/projeto-spring-teste/src/main/java/com/consoli/projeto_spring_teste/ProjetoSpringTesteApplication.java
@@ -0,0 +1,13 @@
+package com.consoli.projeto_spring_teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringTesteApplication.class, args);
+	}
+
+}
---
+++ projeto-spring-teste/projeto-spring-teste/src/test/java/com/consoli/projeto_spring_teste/ProjetoSpringTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consoli.projeto_spring_teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

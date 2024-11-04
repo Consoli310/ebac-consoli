017b0a5256205baef66294ece689b4e2e601367a
Consoli310 <matheusconsoli310@gmail.com>
2024-11-03 22:50:23 -0300

    third commit

--- javapro-mod2/src/FatorialBottomUp.java
+++ javapro-mod2/src/FatorialBottomUp.java
@@ -2,7 +2,7 @@ public class FatorialBottomUp {
 
     public static int fatorial(int n) {
         int[] tabela = new int[n + 1];
-        tabela[0] = 1; 
+        tabela[0] = 1;
         tabela[1] = 1;
 
         for (int i = 2; i <= n; i++) {
@@ -12,7 +12,7 @@ public class FatorialBottomUp {
     }
 
     public static void main(String[] args) {
-        System.out.println("Fatorial de 3 (Bottom-Up): " + fatorial(3)); // Saída esperada: 6
-        System.out.println("Fatorial de 7 (Bottom-Up): " + fatorial(7)); // Saída esperada: 5040
+        System.out.println("Fatorial de 3 (Bottom-Up): " + fatorial(3));
+        System.out.println("Fatorial de 7 (Bottom-Up): " + fatorial(7));
     }
 }
